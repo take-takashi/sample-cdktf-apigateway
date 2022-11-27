f87777a1acbff5605046d807f807981086712aba
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-11-27 11:58:10 +0000

    fix init.bash

--- .devcontainer/init.bash
+++ .devcontainer/init.bash
@@ -2,5 +2,5 @@
 
 cd ~
 echo "export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_CLIUSER}" >> .bashrc
-echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY_CLIUSER" >> .bashrc
+echo "export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_CLIUSER}" >> .bashrc
 echo "export AWS_REGION=ap-northeast-1" >> .bashrc
