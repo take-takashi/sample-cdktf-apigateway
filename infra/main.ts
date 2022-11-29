d798cc29b9a90732107c34ef918158080f8d4ec2
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-11-29 09:02:31 +0000

    Lambda用のダミーS3オブジェクト配置まで

--- infra/main.ts
+++ infra/main.ts
@@ -4,6 +4,8 @@ import { Construct } from 'constructs'
 import { App, TerraformStack } from 'cdktf'
 import * as aws from '@cdktf/provider-aws'
 
+const projectName = 'sample-cdktf-apigateway'
+
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
     super(scope, id)
@@ -15,9 +17,25 @@ class MyStack extends TerraformStack {
       region: 'ap-northeast-1',
     })
 
+    // API Gatewayの作成
     new aws.apigatewayv2Api.Apigatewayv2Api(this, 'apigatewayv2Api', {
-      name: 'nametest', // API Gatewayの名前になる
+      name: `${projectName}-nametest`, // API Gatewayの名前になる
       protocolType: 'HTTP',
+    })
+
+    // Lambda用のS3バケット作成
+    const s3bucket = new aws.s3Bucket.S3Bucket(this, 's3Bucket', {
+      bucket: `${projectName}-lambda-test-s3`,
+    })
+
+    // Lambdaデプロイ用のS3オブジェクト
+    // 実態なしのダミーファイルとして配置
+    new aws.s3Object.S3Object(this, 's3Object', {
+      bucket: s3bucket.bucket,
+      key: 'lambda/helloworld.zip',
+      lifecycle: {
+        ignoreChanges: ['etag', 'metadata'],
+      },
     })
   }
 }
