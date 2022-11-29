d43cf0b50656ac3a556353ecc5c1bf2e12598555
TAKE Takashi <nakamizo.takashi@gmail.com>
2022-11-29 06:58:12 +0000

    add .prettierrc add infra resource

--- infra/main.ts
+++ infra/main.ts
@@ -1,16 +1,27 @@
 // Copyright (c) HashiCorp, Inc
 // SPDX-License-Identifier: MPL-2.0
-import { Construct } from "constructs";
-import { App, TerraformStack } from "cdktf";
+import { Construct } from 'constructs'
+import { App, TerraformStack } from 'cdktf'
+import * as aws from '@cdktf/provider-aws'
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, id: string) {
-    super(scope, id);
+    super(scope, id)
 
     // define resources here
+
+    // aws provider (default)
+    new aws.provider.AwsProvider(this, 'aws', {
+      region: 'ap-northeast-1',
+    })
+
+    new aws.apigatewayv2Api.Apigatewayv2Api(this, 'apigatewayv2Api', {
+      name: 'nametest',
+      protocolType: 'HTTP',
+    })
   }
 }
 
-const app = new App();
-new MyStack(app, "infra");
-app.synth();
+const app = new App()
+new MyStack(app, 'infra')
+app.synth()
